48929407e6a8f649506a4552ed7b45a9f6982e3c
Igor <example@gmail.com>
2022-03-19 12:38:48 +0700

    First Pratice

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastWatchedFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      feedback = prompt('На сколько оцените его?', ''),
+      lastWatchedFilm2 = prompt('Один из последних просмотренных фильмов?'),
+      feedback2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWatchedFilm] = feedback;
+personalMovieDB.movies[lastWatchedFilm2] = feedback2;
+
+console.log(personalMovieDB);
+
+
+
+
